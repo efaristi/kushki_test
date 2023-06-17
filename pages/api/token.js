e0a4c85b482185c3dfd1cc91f9dbd16db2993316
Esteban Aristizabal <felipedc9987@hotmail.com>
2023-06-17 16:34:01 -0500

    refactorización del servicio

--- pages/api/token.js
+++ pages/api/token.js
@@ -1,22 +1,12 @@
 import { Kushki } from "@kushki/js";
 
+const kushki = new Kushki({
+  merchantId: 'd79d2ac25c104ee384085c8b73627e1f',
+  inTestEnvironment: true,
+  regional: false
+});
+
 export default function handler(req, res) {
-  var callback = function (response) {
-    if (!response.code) {
-      console.log(response);
-      res.status(200).json({ response })
-
-    } else {
-      console.error('Error: ', response.error, 'Code: ', response.code, 'Message: ', response.message);
-      res.status(400).json({ response })
-    }
-  }
-
-  var kushki = new Kushki({
-    merchantId: 'd79d2ac25c104ee384085c8b73627e1f', 
-    inTestEnvironment: true,
-    regional:false
-  });
 
   kushki.requestToken({
     amount: '49.99',
@@ -28,6 +18,15 @@ export default function handler(req, res) {
       expiryMonth: "12",
       expiryYear: "28"
     },
-  }, callback); // Also you can set the function directly
+  }, (response) => {
+    if (!response.code) {
+      console.log(response);
+      res.status(200).json({ response })
+
+    } else {
+      console.error('Error: ', response.error, 'Code: ', response.code, 'Message: ', response.message);
+      res.status(400).json({ response })
+    }
+  }); // Also you can set the function directly
 
 }
