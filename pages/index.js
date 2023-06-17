ceab27eb77f6ef5da32d8fff17f59723d8141622
Esteban Aristizabal <felipedc9987@hotmail.com>
2023-06-17 17:30:38 -0500

    ejemplo recolección datos formulario

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,32 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const pay = (evt) => {
+    const {
+      cvc: cvc,
+      expiry_month: expiryMonth,
+      expiry_year: expiryYear,
+      name: name,
+      number: cardNumber
+    } = evt.target.form.elements
+
+    console.log({
+      cvc:cvc.value,
+      expiryMonth:expiryMonth.value,
+      expiryYear:expiryYear.value,
+      name:name.value,
+      cardNumber:cardNumber.value
+    });
+    evt.preventDefault()
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div className="relative flex place-items-center">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -20,7 +40,17 @@ export default function Home() {
         />
       </div>
 
-      <div>Aquí vamos a poner el formulario</div>
+      <div>
+        <form id="formulario-de-pago">
+          <input placeholder="Card Number" type="text" name="number" />
+          <input placeholder="Full Name" type="text" name="name" />
+          <input placeholder="MM" type="text" name="expiry_month" />
+          <input placeholder="YY" type="text" name="expiry_year" />
+          <input placeholder="CVC" type="text" name="cvc" />
+
+          <button onClick={(evt) => pay(evt)}>Pay $14.99</button>
+        </form>
+      </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
